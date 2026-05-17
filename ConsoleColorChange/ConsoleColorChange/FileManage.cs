826870666c5f4eb32b7176acdaf9fd964b34e8b1
Blamar99 <blamar99@protonmail.com>
2026-05-17 11:24:04 +0200

    save and load
    in this "update" your color will get a save and when you open the console it will load the last used color.

---
+++ ConsoleColorChange/ConsoleColorChange/FileManage.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ConsoleColorChange
+{
+    internal class FileManage
+    {
+        private string filePath = "D:\\Visual Studio\\C# projects\\ConsoleColorChange\\ConsoleColorChange\\bin\\Debug\\net10.0\\save.txt";
+        public void LoadSaves()
+        {
+            
+            String line;
+            if(File.Exists(filePath))
+            {
+                StreamReader sr = new StreamReader(filePath);
+                line = sr.ReadLine();
+                sr.Close();
+            }
+            else
+            {
+                File.Create(filePath);
+                StreamReader sr = new StreamReader(filePath);
+                line = sr.ReadLine();
+                sr.Close();
+            }
+
+            switch (line)
+            {
+                case "sv_red":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case "sv_white":
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case "sv_blue":
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+                case "sv_green":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+            }
+        }
+
+        public void DeleteFile()
+        {
+            if(File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+        }
+
+        public void WriteFile(string input)
+        {
+            StreamWriter sw = new StreamWriter(filePath);
+            switch(input)
+            {
+                case "/red":
+                    sw.WriteLine("sv_red");
+                    break;
+                case "/white":
+                    sw.WriteLine("sv_white");
+                    break;
+                case "/blue":
+                    sw.WriteLine("sv_blue");
+                    break;
+                case "/green":
+                    sw.WriteLine("sv_green");
+                    break;
+            }
+
+            sw.Close();
+        }
+    }
+}
--- ConsoleColorChange/ConsoleColorChange/Program.cs
+++ ConsoleColorChange/ConsoleColorChange/Program.cs
@@ -1,38 +1,67 @@
-﻿namespace ConsoleColorChange
+﻿using System;
+using System.IO;
+using System.ComponentModel;
+using System.Diagnostics;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ConsoleColorChange
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            FileManage f = new FileManage();
+            f.LoadSaves();
+
             string input;
+            //string currentApplication = Process.GetCurrentProcess().MainModule.FileName; //We get the file's location (and it's name) for the restart.
             bool exit = false;
             Console.WriteLine("Welcome in the Console!\n");
             Console.WriteLine("Write /help for the commands.");
-                input = Console.ReadLine();
+            //Console.WriteLine(currentApplication);
+            input = Console.ReadLine();
             Console.Clear();
+            Console.WriteLine(input);
             while(!exit)
             {
-
+                
                 switch(input)
                 {
                     case "/red":
+                        f.DeleteFile();
                         Console.ForegroundColor = ConsoleColor.Red;
+                        f.WriteFile(input);
+                        break;
+                    case "/white":
+                        f.DeleteFile();
+                        Console.ForegroundColor = ConsoleColor.White;
+                        f.WriteFile(input);
                         break;
                     case "/blue":
+                        f.DeleteFile();
                         Console.ForegroundColor = ConsoleColor.Blue;
+                        f.WriteFile(input);
                         break;
                     case "/green":
+                        f.DeleteFile();
                         Console.ForegroundColor = ConsoleColor.Green;
+                        f.WriteFile(input);
                         break;
                     case "/help":
                         Console.WriteLine("Console commands:\n/red - Change the font color to red.");
                         Console.WriteLine("/blue - Change the font color to blue.");
                         Console.WriteLine("/green - Change the font color to green.");
+                        Console.WriteLine("/white - Change the font color to white.\n");
                         Console.WriteLine("/clear - Clear the Console.\n");
+                        Console.WriteLine("/restart - Restart the program.");
                         Console.WriteLine("/exit - Leave the program.");
                         break;
                     case "/exit":
                         exit = true;
+                        break;
+                    case "/restart":
+                        Process.Start(Environment.ProcessPath); //alternative variable for get the location of the file on line 15
+                        Environment.Exit(0);
                         break;
                     case "/clear":
                         Console.Clear();
